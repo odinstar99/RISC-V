83c8f2f45b6e6d3ff678f401b63f2fa069554253
Michiel Visser <info@webmichiel.nl>
2018-03-08 19:50:48 +0100

    Fully implement unaligned memory access
    Memory accesses that cross word borders will incur a one cycle delay for
    reading or writing, since this action is split up into two different
    reads or writes.

--- asm/align.c
+++ asm/align.c
@@ -2,63 +2,126 @@
 #define HEX ((volatile unsigned int *) 0x70000004)
 #define SWITCH ((volatile unsigned int *) 0x70000008)
 
-unsigned int last_switch_state;
+unsigned int __attribute__ ((noinline)) lw(unsigned char *addr) {
+    unsigned int result;
+    asm volatile ("lw %0, 0(%1)" : "=r"(result) : "r"(addr));
+    return result;
+}
 
-void wait_for_change() {
-    unsigned int read_switch;
-    while ((read_switch = *SWITCH) == last_switch_state) {
-        for (int i = 0; i < 100000; i++) asm volatile ("nop");
+unsigned short __attribute__ ((noinline)) lhu(unsigned char *addr) {
+    unsigned short result;
+    asm volatile ("lhu %0, 0(%1)" : "=r"(result) : "r"(addr));
+    return result;
+}
+
+void compare_byte(unsigned char a, unsigned char b, unsigned int line) {
+    if (a != b) {
+        *HEX = (0xff << 16) | line;
+        while (1);
     }
-    last_switch_state = read_switch;
+}
+
+void compare_halfword(unsigned short a, unsigned short b, unsigned int line) {
+    if (a != b) {
+        *HEX = (0xff << 16) | line;
+        while (1);
+    }
+}
+
+void compare_word(unsigned int a, unsigned int b, unsigned int line) {
+    if (a != b) {
+        *HEX = (0xff << 16) | line;
+        while (1);
+    }
 }
 
 unsigned char data[] = {0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef};
+unsigned char empty[8] = {0};
 
 void main() {
-    last_switch_state = *SWITCH;
+    compare_byte(data[0], 0x12, __LINE__);
+    compare_byte(data[1], 0x34, __LINE__);
+    compare_byte(data[2], 0x56, __LINE__);
+    compare_byte(data[3], 0x78, __LINE__);
+    compare_byte(data[4], 0x90, __LINE__);
+    compare_byte(data[5], 0xab, __LINE__);
+    compare_byte(data[6], 0xcd, __LINE__);
+    compare_byte(data[7], 0xef, __LINE__);
 
-    *HEX = data[0];
-    wait_for_change();
-    *HEX = data[1];
-    wait_for_change();
-    *HEX = data[2];
-    wait_for_change();
-    *HEX = data[3];
-    wait_for_change();
-    *HEX = data[4];
-    wait_for_change();
-    *HEX = data[5];
-    wait_for_change();
-    *HEX = data[6];
-    wait_for_change();
-    *HEX = data[7];
-    wait_for_change();
+    compare_halfword(lhu(data + 0), 0x3412, __LINE__);
+    compare_halfword(lhu(data + 1), 0x5634, __LINE__);
+    compare_halfword(lhu(data + 2), 0x7856, __LINE__);
+    compare_halfword(lhu(data + 3), 0x9078, __LINE__);
+    compare_halfword(lhu(data + 4), 0xab90, __LINE__);
+    compare_halfword(lhu(data + 5), 0xcdab, __LINE__);
+    compare_halfword(lhu(data + 6), 0xefcd, __LINE__);
 
-    *HEX = *((unsigned short *) (data + 0));
-    wait_for_change();
-    *HEX = *((unsigned short *) (data + 1));
-    wait_for_change();
-    *HEX = *((unsigned short *) (data + 2));
-    wait_for_change();
-    *HEX = *((unsigned short *) (data + 3));
-    wait_for_change();
-    *HEX = *((unsigned short *) (data + 4));
-    wait_for_change();
-    *HEX = *((unsigned short *) (data + 5));
-    wait_for_change();
-    *HEX = *((unsigned short *) (data + 6));
-    wait_for_change();
+    compare_word(lw(data + 0), 0x78563412, __LINE__);
+    compare_word(lw(data + 1), 0x90785634, __LINE__);
+    compare_word(lw(data + 2), 0xab907856, __LINE__);
+    compare_word(lw(data + 3), 0xcdab9078, __LINE__);
+    compare_word(lw(data + 4), 0xefcdab90, __LINE__);
 
-    *HEX = *((unsigned int *) (data + 0));
-    wait_for_change();
-    *HEX = *((unsigned int *) (data + 1));
-    wait_for_change();
-    *HEX = *((unsigned int *) (data + 2));
-    wait_for_change();
-    *HEX = *((unsigned int *) (data + 3));
-    wait_for_change();
-    *HEX = *((unsigned int *) (data + 4));
-    wait_for_change();
+    unsigned int *empty_ptr = (unsigned int *) empty;
+    *((unsigned int *) (empty + 0)) = 0x12345678;
+    compare_word(lw(empty + 0), 0x12345678, __LINE__);
+    compare_word(lw(empty + 4), 0x00000000, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned int *) (empty + 1)) = 0x12345678;
+    compare_word(lw(empty + 0), 0x34567800, __LINE__);
+    compare_word(lw(empty + 4), 0x00000012, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned int *) (empty + 2)) = 0x12345678;
+    compare_word(lw(empty + 0), 0x56780000, __LINE__);
+    compare_word(lw(empty + 4), 0x00001234, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned int *) (empty + 3)) = 0x12345678;
+    compare_word(lw(empty + 0), 0x78000000, __LINE__);
+    compare_word(lw(empty + 4), 0x00123456, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned int *) (empty + 4)) = 0x12345678;
+    compare_word(lw(empty + 0), 0X00000000, __LINE__);
+    compare_word(lw(empty + 4), 0x12345678, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 0)) = 0x1234;
+    compare_word(lw(empty + 0), 0X00001234, __LINE__);
+    compare_word(lw(empty + 4), 0x00000000, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 1)) = 0x1234;
+    compare_word(lw(empty + 0), 0X00123400, __LINE__);
+    compare_word(lw(empty + 4), 0x00000000, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 2)) = 0x1234;
+    compare_word(lw(empty + 0), 0X12340000, __LINE__);
+    compare_word(lw(empty + 4), 0x00000000, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 3)) = 0x1234;
+    compare_word(lw(empty + 0), 0X34000000, __LINE__);
+    compare_word(lw(empty + 4), 0x00000012, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 4)) = 0x1234;
+    compare_word(lw(empty + 0), 0X00000000, __LINE__);
+    compare_word(lw(empty + 4), 0x00001234, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 5)) = 0x1234;
+    compare_word(lw(empty + 0), 0X00000000, __LINE__);
+    compare_word(lw(empty + 4), 0x00123400, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
+
+    *((unsigned short *) (empty + 6)) = 0x1234;
+    compare_word(lw(empty + 0), 0X00000000, __LINE__);
+    compare_word(lw(empty + 4), 0x12340000, __LINE__);
+    empty_ptr[0] = 0; empty_ptr[1] = 0;
 
     *HEX = 0x888888;
 }
--- rtl/memory.sv
+++ rtl/memory.sv
@@ -1,3 +1,8 @@
+typedef enum {
+    NORMAL,
+    UNALIGNED
+} memory_state_t;
+
 module memory(
     input clk,
     input reset_n,
@@ -24,7 +29,7 @@ module memory(
 
 rom rom0 (
     .address_a(imem_address[15:2]),
-    .address_b(dmem_address[15:2]),
+    .address_b(selected_dmem_address[15:2]),
     .clock_a(clk),
     .clock_b(clk),
     .enable_a(imem_enable),
@@ -34,7 +39,7 @@ rom rom0 (
 );
 
 ram ram0 (
-    .address(dmem_address[15:2]),
+    .address(selected_dmem_address[15:2]),
     .byteena(ram_byteen),
     .clken(ram_enable),
     .clock(clk),
@@ -45,7 +50,7 @@ ram ram0 (
 
 mmio mmio0 (
     .reset_n(reset_n),
-    .address(dmem_address[15:2]),
+    .address(selected_dmem_address[15:2]),
     .byteena(mmio_byteen),
     .clken(mmio_enable),
     .clock(clk),
@@ -57,6 +62,7 @@ mmio mmio0 (
     .switch(switch)
 );
 
+logic [31:0] selected_dmem_address;
 // ROM control signals
 logic rom_enable;
 // RAM control signals
@@ -76,8 +82,15 @@ logic [31:0] ram_q;
 logic [31:0] mmio_q;
 
 // Internal state storage
+memory_state_t memory_state;
+memory_state_t next_memory_state;
 logic [31:0] last_dmem_address;
 logic [2:0] last_dmem_read_mode;
+logic [31:0] last_dmem_read_data;
+logic [2:0] last_dmem_write_mode;
+logic [31:0] last_dmem_write_data;
+logic last_dmem_enable;
+logic last_dmem_write_enable;
 
 always begin
     // Internal lines
@@ -87,129 +100,236 @@ always begin
 
     imem_wait = 0;
     dmem_wait = 0;
+    next_memory_state = NORMAL;
 
-    //
-    // INPUT SIDE
-    //
+    if (memory_state == NORMAL) begin
+        //
+        // INPUT SIDE
+        //
 
-    // Set up the write data and the byte enable
-    dmem_write_data_internal = 0;
-    dmem_byteen_internal = 0;
-    if (dmem_write_mode[1:0] == 0) begin
-        // Store byte
-        case (dmem_address[1:0])
-            0: dmem_write_data_internal = {24'h0, dmem_write_data[7:0]};
-            1: dmem_write_data_internal = {16'h0, dmem_write_data[7:0], 8'h0};
-            2: dmem_write_data_internal = {8'h0, dmem_write_data[7:0], 16'h0};
-            3: dmem_write_data_internal = {dmem_write_data[7:0], 24'h0};
+        // Set up the write data and the byte enable
+        dmem_write_data_internal = 0;
+        dmem_byteen_internal = 0;
+        if (dmem_write_mode[1:0] == 0) begin
+            // Store byte
+            case (dmem_address[1:0])
+                0: dmem_write_data_internal = {24'h0, dmem_write_data[7:0]};
+                1: dmem_write_data_internal = {16'h0, dmem_write_data[7:0], 8'h0};
+                2: dmem_write_data_internal = {8'h0, dmem_write_data[7:0], 16'h0};
+                3: dmem_write_data_internal = {dmem_write_data[7:0], 24'h0};
+            endcase
+            dmem_byteen_internal = (4'b1 << dmem_address[1:0]);
+        end else if (dmem_write_mode[1:0] == 1) begin
+            // Store halfword
+            case (dmem_address[1:0])
+                0: dmem_write_data_internal = {16'h0, dmem_write_data[15:0]};
+                1: dmem_write_data_internal = {8'h0, dmem_write_data[15:0], 8'h0};
+                2: dmem_write_data_internal = {dmem_write_data[15:0], 16'h0};
+                3: dmem_write_data_internal = {dmem_write_data[7:0], 24'h0};
+            endcase
+            case (dmem_address[1:0])
+                0: dmem_byteen_internal = 4'b0011;
+                1: dmem_byteen_internal = 4'b0110;
+                2: dmem_byteen_internal = 4'b1100;
+                3: dmem_byteen_internal = 4'b1000;
+            endcase
+        end else if (dmem_write_mode[1:0] == 2) begin
+            // Store word
+            case (dmem_address[1:0])
+                0: dmem_write_data_internal = dmem_write_data;
+                1: dmem_write_data_internal = {dmem_write_data[23:0], 8'h0};
+                2: dmem_write_data_internal = {dmem_write_data[15:0], 16'h0};
+                3: dmem_write_data_internal = {dmem_write_data[7:0], 24'h0};
+            endcase
+            case (dmem_address[1:0])
+                0: dmem_byteen_internal = 4'b1111;
+                1: dmem_byteen_internal = 4'b1110;
+                2: dmem_byteen_internal = 4'b1100;
+                3: dmem_byteen_internal = 4'b1000;
+            endcase
+        end
+
+        if (dmem_write_enable && ((dmem_write_mode[1:0] == 1 && dmem_address[1:0] == 3) || (dmem_write_mode[1:0] == 2 && dmem_address[1:0] != 0)) ||
+            dmem_read_enable && ((dmem_read_mode[1:0] == 1 && dmem_address[1:0] == 3) || (dmem_read_mode[1:0] == 2 && dmem_address[1:0] != 0)))
+        begin
+            next_memory_state = UNALIGNED;
+        end
+
+        selected_dmem_address = dmem_address;
+        // Default ROM settings
+        rom_enable = 0;
+        // Default RAM settings
+        ram_enable = 0;
+        ram_write_enable = 0;
+        ram_write_data = 0;
+        ram_byteen = 0;
+        // Default MMIO settings
+        mmio_enable = 0;
+        mmio_write_enable = 0;
+        mmio_write_data = 0;
+        mmio_byteen = 0;
+
+        // Select the correct inputs for each memory region
+        if (selected_dmem_address[31:16] == 16'h8000) begin
+            // RAM
+            ram_enable = dmem_enable;
+            ram_write_enable = dmem_write_enable;
+            ram_write_data = dmem_write_data_internal;
+            ram_byteen = dmem_byteen_internal;
+        end else if (selected_dmem_address[31:16] == 16'h0000) begin
+            // ROM
+            rom_enable = dmem_enable;
+        end else if (selected_dmem_address[31:16] == 16'h7000) begin
+            mmio_enable = dmem_enable;
+            mmio_write_enable = dmem_write_enable;
+            mmio_write_data = dmem_write_data_internal;
+            mmio_byteen = dmem_byteen_internal;
+        end
+
+        //
+        // OUTPUT SIDE
+        //
+        case (last_dmem_address[31:16])
+            16'h0000: dmem_read_data_internal = rom_q;
+            16'h7000: dmem_read_data_internal = mmio_q;
+            16'h8000: dmem_read_data_internal = ram_q;
+            default: dmem_read_data_internal = 0;
         endcase
-        dmem_byteen_internal = (4'b1 << dmem_address[1:0]);
-    end else if (dmem_write_mode[1:0] == 1) begin
-        // Store halfword
-        case (dmem_address[1:0])
-            0: dmem_write_data_internal = {16'h0, dmem_write_data[15:0]};
-            1: dmem_write_data_internal = {8'h0, dmem_write_data[15:0], 8'h0};
-            2: dmem_write_data_internal = {dmem_write_data[15:0], 16'h0};
-            3: dmem_write_data_internal = 0;
+
+        // Select the correct bytes from the output
+        dmem_read_data = 0;
+        if (last_dmem_read_mode[1:0] == 0) begin
+            // Read byte
+            case (last_dmem_address[1:0])
+                0: dmem_read_data = {24'h0, dmem_read_data_internal[7:0]};
+                1: dmem_read_data = {24'h0, dmem_read_data_internal[15:8]};
+                2: dmem_read_data = {24'h0, dmem_read_data_internal[23:16]};
+                3: dmem_read_data = {24'h0, dmem_read_data_internal[31:24]};
+            endcase
+         end else if (last_dmem_read_mode[1:0] == 1) begin
+            // Read halfword
+            case (last_dmem_address[1:0])
+                0: dmem_read_data = {16'h0, dmem_read_data_internal[15:0]};
+                1: dmem_read_data = {16'h0, dmem_read_data_internal[23:8]};
+                2: dmem_read_data = {16'h0, dmem_read_data_internal[31:16]};
+                3: dmem_read_data = {16'h0, dmem_read_data_internal[7:0], last_dmem_read_data[31:24]};
+            endcase
+         end else if (last_dmem_read_mode[1:0] == 2) begin
+            // Read word
+            case (last_dmem_address[1:0])
+                0: dmem_read_data = dmem_read_data_internal;
+                1: dmem_read_data = {dmem_read_data_internal[7:0], last_dmem_read_data[31:8]};
+                2: dmem_read_data = {dmem_read_data_internal[15:0], last_dmem_read_data[31:16]};
+                3: dmem_read_data = {dmem_read_data_internal[23:0], last_dmem_read_data[31:24]};
+            endcase
+         end
+    end else begin
+        //
+        // INPUT SIDE
+        //
+        dmem_wait = 1;
+
+        // Set up the write data and the byte enable
+        dmem_write_data_internal = 0;
+        dmem_byteen_internal = 0;
+        if (last_dmem_write_mode[1:0] == 0) begin
+            // Store byte, this should never be the case
+            dmem_write_data_internal = 0;
+            dmem_byteen_internal = 0;
+        end else if (last_dmem_write_mode[1:0] == 1) begin
+            // Store halfword
+            if (last_dmem_address[1:0] == 3) begin
+                dmem_write_data_internal = {24'h0, last_dmem_write_data[15:8]};
+                dmem_byteen_internal = 4'b0001;
+            end else begin
+                dmem_write_data_internal = 0;
+                dmem_byteen_internal = 0;
+            end
+        end else if (last_dmem_write_mode[1:0] == 2) begin
+            // Store word
+            case (last_dmem_address[1:0])
+                0: dmem_write_data_internal = 0;
+                1: dmem_write_data_internal = {24'h0, last_dmem_write_data[31:24]};
+                2: dmem_write_data_internal = {16'h0, last_dmem_write_data[31:16]};
+                3: dmem_write_data_internal = {8'h0, last_dmem_write_data[31:8]};
+            endcase
+            case (last_dmem_address[1:0])
+                0: dmem_byteen_internal = 0;
+                1: dmem_byteen_internal = 4'b0001;
+                2: dmem_byteen_internal = 4'b0011;
+                3: dmem_byteen_internal = 4'b0111;
+            endcase
+        end
+
+        selected_dmem_address = last_dmem_address + 32'h4;
+        // Default ROM settings
+        rom_enable = 0;
+        // Default RAM settings
+        ram_enable = 0;
+        ram_write_enable = 0;
+        ram_write_data = 0;
+        ram_byteen = 0;
+        // Default MMIO settings
+        mmio_enable = 0;
+        mmio_write_enable = 0;
+        mmio_write_data = 0;
+        mmio_byteen = 0;
+
+        // Select the correct inputs for each memory region
+        if (selected_dmem_address[31:16] == 16'h8000) begin
+            // RAM
+            ram_enable = last_dmem_enable;
+            ram_write_enable = last_dmem_write_enable;
+            ram_write_data = dmem_write_data_internal;
+            ram_byteen = dmem_byteen_internal;
+        end else if (selected_dmem_address[31:16] == 16'h0000) begin
+            // ROM
+            rom_enable = last_dmem_enable;
+        end else if (selected_dmem_address[31:16] == 16'h7000) begin
+            mmio_enable = last_dmem_enable;
+            mmio_write_enable = last_dmem_write_enable;
+            mmio_write_data = dmem_write_data_internal;
+            mmio_byteen = dmem_byteen_internal;
+        end
+
+        //
+        // OUTPUT SIDE
+        //
+        case (last_dmem_address[31:16])
+            16'h0000: dmem_read_data_internal = rom_q;
+            16'h7000: dmem_read_data_internal = mmio_q;
+            16'h8000: dmem_read_data_internal = ram_q;
+            default: dmem_read_data_internal = 0;
         endcase
-        case (dmem_address[1:0])
-            0: dmem_byteen_internal = 4'b0011;
-            1: dmem_byteen_internal = 4'b0110;
-            2: dmem_byteen_internal = 4'b1100;
-            3: dmem_byteen_internal = 4'b0000;
-        endcase
-    end else if (dmem_write_mode[1:0] == 2) begin
-        // Store word
-        case (dmem_address[1:0])
-            0: dmem_write_data_internal = dmem_write_data;
-            1: dmem_write_data_internal = 0;
-            2: dmem_write_data_internal = 0;
-            3: dmem_write_data_internal = 0;
-        endcase
-        case (dmem_address[1:0])
-            0: dmem_byteen_internal = 4'b1111;
-            1: dmem_byteen_internal = 4'b0000;
-            2: dmem_byteen_internal = 4'b0000;
-            3: dmem_byteen_internal = 4'b0000;
-        endcase
+
+        // Select the correct bytes from the output
+        dmem_read_data = dmem_read_data_internal;
     end
-
-    // Default ROM settings
-    rom_enable = 0;
-    // Default RAM settings
-    ram_enable = 0;
-    ram_write_enable = 0;
-    ram_write_data = 0;
-    ram_byteen = 0;
-    // Default MMIO settings
-    mmio_enable = 0;
-    mmio_write_enable = 0;
-    mmio_write_data = 0;
-    mmio_byteen = 0;
-
-    // Select the correct inputs for each memory region
-    if (dmem_address[31:16] == 16'h8000) begin
-        // RAM
-        ram_enable = dmem_enable;
-        ram_write_enable = dmem_write_enable;
-        ram_write_data = dmem_write_data_internal;
-        ram_byteen = dmem_byteen_internal;
-    end else if (dmem_address[31:16] == 16'h0000) begin
-        // ROM
-        rom_enable = dmem_enable;
-    end else if (dmem_address[31:16] == 16'h7000) begin
-        mmio_enable = dmem_enable;
-        mmio_write_enable = dmem_write_enable;
-        mmio_write_data = dmem_write_data_internal;
-        mmio_byteen = dmem_byteen_internal;
-    end
-
-    //
-    // OUTPUT SIDE
-    //
-    case (last_dmem_address[31:16])
-        16'h0000: dmem_read_data_internal = rom_q;
-        16'h7000: dmem_read_data_internal = mmio_q;
-        16'h8000: dmem_read_data_internal = ram_q;
-        default: dmem_read_data_internal = 0;
-    endcase
-
-    // Select the correct bytes from the output
-    dmem_read_data = 0;
-    if (last_dmem_read_mode[1:0] == 0) begin
-        // Read byte
-        case (last_dmem_address[1:0])
-            0: dmem_read_data = {24'h0, dmem_read_data_internal[7:0]};
-            1: dmem_read_data = {24'h0, dmem_read_data_internal[15:8]};
-            2: dmem_read_data = {24'h0, dmem_read_data_internal[23:16]};
-            3: dmem_read_data = {24'h0, dmem_read_data_internal[31:24]};
-        endcase
-     end else if (last_dmem_read_mode[1:0] == 1) begin
-        // Read halfword
-        case (last_dmem_address[1:0])
-            0: dmem_read_data = {16'h0, dmem_read_data_internal[15:0]};
-            1: dmem_read_data = {16'h0, dmem_read_data_internal[23:7]};
-            2: dmem_read_data = {16'h0, dmem_read_data_internal[31:16]};
-            3: dmem_read_data = 0;
-        endcase
-     end else if (last_dmem_read_mode[1:0] == 2) begin
-        // Read word
-        case (last_dmem_address[1:0])
-            0: dmem_read_data = dmem_read_data_internal;
-            1: dmem_read_data = 0;
-            2: dmem_read_data = 0;
-            3: dmem_read_data = 0;
-        endcase
-     end
 end
 
 always @(posedge clk) begin
     if (!reset_n) begin
+        memory_state <= NORMAL;
         last_dmem_address <= 0;
         last_dmem_read_mode <= 0;
+        last_dmem_read_data <= 0;
+        last_dmem_write_mode <= 0;
+        last_dmem_write_data <= 0;
+        last_dmem_enable <= 0;
+        last_dmem_write_enable <= 0;
     end else begin
-        last_dmem_address <= dmem_address;
-        last_dmem_read_mode <= dmem_read_mode;
+        memory_state <= next_memory_state;
+        if (memory_state == NORMAL) begin
+            last_dmem_read_mode <= dmem_read_mode;
+        end else begin
+            last_dmem_read_mode <= last_dmem_read_mode;
+        end
+        last_dmem_address <= selected_dmem_address;
+        last_dmem_read_data <= dmem_read_data;
+        last_dmem_write_mode <= dmem_write_mode;
+        last_dmem_write_data <= dmem_write_data;
+        last_dmem_enable <= dmem_enable;
+        last_dmem_write_enable <= dmem_write_enable;
      end
 end
 
